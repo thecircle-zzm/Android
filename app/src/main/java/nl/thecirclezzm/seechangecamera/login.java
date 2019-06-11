c1abd02a1698e26e0f5b254a2d809a03a315cfdd
ItzJasonnn <jasonvana@ziggo.nl>
2019-06-11 13:16:56 +0200

    MD5 Test

--- app/src/main/java/nl/thecirclezzm/seechangecamera/login.java
+++ app/src/main/java/nl/thecirclezzm/seechangecamera/login.java
@@ -8,6 +8,8 @@ import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
 
+import java.math.BigInteger;
+
 public class login extends AppCompatActivity {
 
     @Override
@@ -18,8 +20,8 @@ public class login extends AppCompatActivity {
         TextView usernameTextView = findViewById(R.id.usernameTextView);
         TextView passwordTextView = findViewById(R.id.passwordTextView);
 
-        EditText usernameInputField = findViewById(R.id.usernameInputField);
-        EditText passwordInputField = findViewById(R.id.passwordInputField);
+//        EditText usernameInputField = findViewById(R.id.usernameInputField);
+//        EditText passwordInputField = findViewById(R.id.passwordInputField);
 
         Button loginButton = findViewById(R.id.loginButton);
     }
@@ -27,7 +29,20 @@ public class login extends AppCompatActivity {
     public void loginButton(View view){
         EditText usernameInputField = findViewById(R.id.usernameInputField);
         EditText passwordInputField = findViewById(R.id.passwordInputField);
+        TextView passwordTextView = findViewById(R.id.passwordTextView);
+
 
         byte[] md5Input = passwordInputField.getText().toString().getBytes();
+        BigInteger md5Data = null;
+
+        try {
+            md5Data = new BigInteger(1, md5.encryptMD5(md5Input));
+        } catch(Exception e){
+            e.printStackTrace();
+        }
+
+        String md55tr = md5Data.toString(16);
+
+        passwordTextView.setText(md55tr);
     }
 }
