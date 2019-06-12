bcb342ae392d9854246a4dacbbc1d1855bb47510
ItzJasonnn <jasonvana@ziggo.nl>
2019-06-12 11:25:29 +0200

    MD5 removed

--- app/src/main/java/nl/thecirclezzm/seechangecamera/login.java
+++ app/src/main/java/nl/thecirclezzm/seechangecamera/login.java
@@ -26,23 +26,5 @@ public class login extends AppCompatActivity {
         Button loginButton = findViewById(R.id.loginButton);
     }
 
-    public void loginButton(View view){
-        EditText usernameInputField = findViewById(R.id.usernameInputField);
-        EditText passwordInputField = findViewById(R.id.passwordInputField);
-        TextView passwordTextView = findViewById(R.id.passwordTextView);
-
-
-        byte[] md5Input = passwordInputField.getText().toString().getBytes();
-        BigInteger md5Data = null;
-
-        try {
-            md5Data = new BigInteger(1, md5.encryptMD5(md5Input));
-        } catch(Exception e){
-            e.printStackTrace();
-        }
-
-        String md55tr = md5Data.toString(16);
-
-        passwordTextView.setText(md55tr);
-    }
+    
 }
