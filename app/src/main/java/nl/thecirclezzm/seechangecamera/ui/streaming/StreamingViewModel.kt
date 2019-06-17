700f9e5374a94ea644e02057e99c9d50ee1ca9bd
Marc <16156117+Marc-JB@users.noreply.github.com>
2019-06-17 12:27:10 +0200

    Fix url

--- app/src/main/java/nl/thecirclezzm/seechangecamera/ui/streaming/StreamingViewModel.kt
+++ app/src/main/java/nl/thecirclezzm/seechangecamera/ui/streaming/StreamingViewModel.kt
@@ -5,7 +5,8 @@ import androidx.lifecycle.ViewModel
 import nl.thecirclezzm.streaming.base.ConnectionCallbacks
 
 class StreamingViewModel : ViewModel(), ConnectionCallbacks {
-    val streamingUrl = "rtmp://188.166.38.127:8000/live/stream"
+    val streamingUrl = "rtmp://188.166.38.127:1935/live/289727c0ba0b9435"
+    //val streamingUrl = "rtmp://145.49.51.243:1935/live/stream"
 
     override fun onConnectionSuccess() {
         Log.i("Stream", "Connection success")
