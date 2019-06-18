fe7e53566283b053a0e9a49ccb0f610b0554fbba
Marc <16156117+Marc-JB@users.noreply.github.com>
2019-06-18 12:38:38 +0200

    Reformat code

--- app/src/main/java/nl/thecirclezzm/seechangecamera/ui/chat/MessageAdapter.java
+++ app/src/main/java/nl/thecirclezzm/seechangecamera/ui/chat/MessageAdapter.java
@@ -7,7 +7,6 @@ import android.util.Log;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
-import android.widget.TextView;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
@@ -25,7 +24,8 @@ public class MessageAdapter extends ArrayAdapter<MessageFormat> {
     }
 
     @Override
-    public @NonNull View getView(int position, @Nullable View convertView, @Nullable ViewGroup parent) {
+    public @NonNull
+    View getView(int position, @Nullable View convertView, @Nullable ViewGroup parent) {
         Log.i(ChatsFragment.TAG, "getView:");
 
         MessageFormat message = getItem(position);
