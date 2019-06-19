fde42fff395d2b3729544c00be74665d21d64b48
ItzJasonnn <jasonvana@ziggo.nl>
2019-06-19 11:15:24 +0200

    Removed stuff

--- app/src/main/java/nl/thecirclezzm/seechangecamera/login.java
+++ app/src/main/java/nl/thecirclezzm/seechangecamera/login.java
@@ -3,24 +3,12 @@ package nl.thecirclezzm.seechangecamera;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
-import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
-import android.widget.ImageView;
 import android.widget.TextView;
 import android.widget.Toast;
 
-import java.io.DataOutputStream;
-import java.io.IOException;
-import java.net.HttpURLConnection;
-import java.net.URL;
-import java.security.KeyStore;
-import java.security.KeyStoreException;
-import java.security.NoSuchAlgorithmException;
-import java.util.Enumeration;
-
 import nl.thecirclezzm.seechangecamera.model.User;
-import nl.thecirclezzm.seechangecamera.model.streamingKey;
 import nl.thecirclezzm.seechangecamera.service.UserClient;
 import retrofit2.Call;
 import retrofit2.Callback;
@@ -35,23 +23,14 @@ public class login extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
 
-        PrintInstalledCertificates();
-
         TextView usernameTextView = findViewById(R.id.usernameTextView);
-        TextView passwordTextView = findViewById(R.id.passwordTextView);
 
         EditText username = findViewById(R.id.usernameInputField);
-        EditText password = findViewById(R.id.passwordInputField);
 
         usernameTextView.setText(username.getText());
-        passwordTextView.setText(password.getText());
 
         Button loginAccount = (Button) findViewById(R.id.loginButton);
         loginAccount.setOnClickListener((view) -> {
-            User user = new User(
-                username.getText().toString(),
-                password.getText().toString()
-            );
 
             sendNetworkRequest(user);
 
@@ -85,47 +64,5 @@ public class login extends AppCompatActivity {
                 Toast.makeText(login.this, "Och jongens nie goed, " + t.toString(), Toast.LENGTH_SHORT).show();
             }
         });
-    }
-
-    public void PrintInstalledCertificates() {
-        try {
-            KeyStore ks = KeyStore.getInstance("AndroidCAStore");
-
-            if (ks != null) {
-                ks.load(null, null);
-                Enumeration<String> aliases = ks.aliases();
-
-                while (aliases.hasMoreElements()) {
-
-                    String alias = (String) aliases.nextElement();
-
-                    java.security.cert.X509Certificate cert = (java.security.cert.X509Certificate) ks.getCertificate(alias);
-                    //To print System Certs only
-                    if(cert.getIssuerDN().getName().contains("system")){
-                        System.out.println(cert.getIssuerDN().getName());
-                    }
-
-                    //To print User Certs only
-                    if(cert.getIssuerDN().getName().contains("user")){
-                        System.out.println(cert.getIssuerDN().getName());
-                    }
-
-                    //To print all certs
-                    System.out.println(cert.getIssuerDN().getName());
-                }
-            }
-        } catch (IOException e) {
-            e.printStackTrace();
-        } catch (KeyStoreException e) {
-            e.printStackTrace();
-        } catch (NoSuchAlgorithmException e) {
-            e.printStackTrace();
-        } catch (java.security.cert.CertificateException e) {
-            e.printStackTrace();
-        }
-    }
-
-    public void loginButtonOnClick(View v){
-
     }
 }
